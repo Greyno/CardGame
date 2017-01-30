ad57514ef5cae6f14216b694aea6991d49c62dae
Gillian Reynolds-Titko <gillianreynolds-titko@zipcoders-MacBook-Pro.local>
2017-01-30 17:16:12 -0500

    refactoring blackjack

--- src/main/java/reynoldstitko/gillian/BlackJackGame.java
+++ src/main/java/reynoldstitko/gillian/BlackJackGame.java
@@ -6,26 +6,32 @@ import java.util.Scanner;
  * Created by gillianreynolds-titko on 1/27/17.
  */
 public class BlackJackGame extends Game {
-
-    Deck cardDeck = new Deck();
     Scanner scanner = new Scanner(System.in);
     Hand playerOneHand = new Hand();
     Hand playerTwoHand = new Hand();
     int playerOneTotal = 0;
     int playerTwoTotal = 0;
-    boolean playerOnePlaying = true;
-    boolean playerTwoPlaying = true;
+
+    Deck cardDeck = new Deck();
 
     public void start() {
 
-        dealABlackJackHand();
+        //Shuffle the deck
+        cardDeck.shuffle();
 
-        showPlayerOneTheirCards();
+        dealTheCards();
+
+        //Show player1 their cards
+        System.out.println("Player 1, you have the following cards:");
+        for (int i = 0; i < playerOneHand.getTheHand().size(); i++) {
+            System.out.print(playerOneHand.getTheHand().get(i) + " \n");
+        }
 
         System.out.println("Player 1, do you want to Hit or Stay?");
-        String response = scanner.next();
 
-        //Play a round with player One
+        boolean playerOnePlaying = true;
+        boolean playerTwoPlaying = false;
+        String response = scanner.next();
 
         if (response.equalsIgnoreCase("Hit") && playerOnePlaying == true) {
             playerOneHand.addCardsToHand(cardDeck.deal(1));
@@ -40,22 +46,25 @@ public class BlackJackGame extends Game {
                 playerOnePlaying = false;
                 playerTwoPlaying = true;
                 playerOneTotal = playerOneHand.sumRankValues(playerOneHand.getTheHand());
+                //break;
             }
-        } else { //Account for a player "staying" on the first turn
+            //Account for player staying in the first move
+        } if(response.equalsIgnoreCase("Stay") && playerOnePlaying == true){
             playerOnePlaying = false;
             playerTwoPlaying = true;
             playerOneTotal = playerOneHand.sumRankValues(playerOneHand.getTheHand());
         }
 
-        showPlayerTwoTheirCards();
+        System.out.println("Player 2, you have the following cards:");
+        for (int i = 0; i < playerTwoHand.getTheHand().size(); i++) {
+            System.out.print(playerTwoHand.getTheHand().get(i) + " \n");
+        }
 
         System.out.println("Player 2, do you want to Hit or Stay?");
         response = scanner.next();
 
-        //Play a round with player Two
         if (response.equalsIgnoreCase("Hit") && playerTwoPlaying == true) {
             //Show player2 their cards
-            System.out.println(playerOnePlaying + "in player2 loop");
             playerTwoHand.addCardsToHand(cardDeck.deal(1));
             System.out.println("Player 2, you have the following cards:");
             for (int i = 0; i < playerTwoHand.getTheHand().size(); i++) {
@@ -68,50 +77,31 @@ public class BlackJackGame extends Game {
                 playerTwoPlaying = false;
                 playerOnePlaying = false;
                 playerTwoTotal = playerTwoHand.sumRankValues(playerTwoHand.getTheHand());
+                //break;
             }
-        } else { //Account for a player "staying" on the first turn
+        } if(response.equalsIgnoreCase("Stay") && playerTwoPlaying == true){
             playerTwoPlaying = false;
             playerOnePlaying = false;
             playerTwoTotal = playerTwoHand.sumRankValues(playerTwoHand.getTheHand());
         }
 
-        //Show game results
-        showGameResults(playerOnePlaying, playerTwoPlaying);
+        endTheGame(false, false);
 
     }
 
-    //--Refactor to provide some helper methods
-    public void showGameResults(boolean playerOneStatus, boolean playerTwoStatus){
-        if (playerOneStatus == false && playerTwoStatus == false) {
-            //Show game results here
-            System.out.print("Player 1 you got " + playerOneTotal + " points\n");
-            System.out.print("Player 2 you got " + playerTwoTotal + " points\n");
-            System.out.println("Game over!");
-        } else System.out.println("Test");
-    }
-
-    public void dealABlackJackHand(){
-        //Shuffle the deck
-        cardDeck.shuffle();
-
-        //Deal 2 cards to each player
+    public void dealTheCards(){
+        //Deal 2 cards to each player, including the dealer
         playerOneHand.addCardsToHand(cardDeck.deal(2));
         playerTwoHand.addCardsToHand(cardDeck.deal(2));
     }
 
-    public void showPlayerOneTheirCards(){
-        System.out.println("Player 1, you have the following cards:");
-        for (int i = 0; i < playerOneHand.getTheHand().size(); i++) {
-            System.out.print(playerOneHand.getTheHand().get(i) + " \n");
+    public void endTheGame(boolean playerOnePlaying, boolean playerTwoPlaying){
+        if (playerOnePlaying == false && playerTwoPlaying == false) {
+            //Show game results here
+            System.out.print("Player 1 you got " + playerOneTotal + " points\n");
+            System.out.print("Player 2 you got " + playerTwoTotal + " points\n");
+            System.out.println("Game over!");
         }
     }
 
-    public void showPlayerTwoTheirCards(){
-        System.out.println("Player 2, you have the following cards:");
-        for (int i = 0; i < playerTwoHand.getTheHand().size(); i++) {
-            System.out.print(playerTwoHand.getTheHand().get(i) + " \n");
-        }
-    }
 }
-
-
